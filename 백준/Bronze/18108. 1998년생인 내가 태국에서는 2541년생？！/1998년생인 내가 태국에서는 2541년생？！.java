7f216938edf1a93177b198a2d50535b4183a6eea
ynoep <114046762+ynoep@users.noreply.github.com>
2023-02-12 12:19:08 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner year = new Scanner(System.in);
+		
+		int byear = year.nextInt();
+		
+		System.out.println(byear-543);
+		
+	}
+
+}
