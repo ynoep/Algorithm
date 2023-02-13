369d9955d88acd5b6df24d2d1b59b67d85e40130
ynoep <114046762+ynoep@users.noreply.github.com>
2023-02-13 21:25:49 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+public class Main {
+	
+	public static void main(String[] args) {
+	
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append("         ,r'\"7\n");
+		sb.append("r`-_   ,'  ,/\n");
+		sb.append(" \\. \". L_r'\n");
+		sb.append("   `~\\/\n");
+		sb.append("      |\n");
+		sb.append("      |\n");
+		
+		System.out.println(sb);
+	}
+}
