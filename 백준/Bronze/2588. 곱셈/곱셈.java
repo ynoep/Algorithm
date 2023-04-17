c2df7ada10577ef89d59fcfcd2374ca4b7d94d36
ynoep <114046762+ynoep@users.noreply.github.com>
2023-04-18 00:41:04 +0900

    [Bronze III] Title: 곱셈, Time: 120 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -7,22 +7,23 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int A = Integer.parseInt(br.readLine()); // 472
-		int B = Integer.parseInt(br.readLine()); // 385
 
-		String StrB = String.valueOf(B);
-		String[] numB = StrB.split("");
+		int A = Integer.parseInt(br.readLine()); // 472
+		String B = br.readLine(); // 385
+
+		String[] numB = B.split("");
 
 		int Ba = Integer.parseInt(numB[2]);
 		int Bb = Integer.parseInt(numB[1]);
 		int Bc = Integer.parseInt(numB[0]);
+		
+		int result1 = A * Ba;
+		int result2 = A * Bb;
+		int result3 = A * Bc;
 
-		System.out.println(A * Ba);
-		System.out.println(A * Bb);
-		System.out.println(A * Bc);
-
-		System.out.println((A * Bc * 100) + (A * Bb * 10) + (A * Ba));
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		System.out.println(result1 + (result2 * 10) + (result3 * 100));
 	}
-
 }
