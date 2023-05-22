8c982122aa591fe78b7f96cdf76136e4e65771c7
yuneeh <114046762+ynoep@users.noreply.github.com>
2023-05-22 21:46:42 +0900

    [Bronze V] Title: N 찍기, Time: 236 ms, Memory: 17852 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -7,14 +7,12 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 
 		int N = Integer.parseInt(br.readLine());
-
-		br.close();
+		StringBuilder sb = new StringBuilder();
 
 		for (int i = 1; i <= N; i++) {
-			sb.append(i + "\n");
+			sb.append(i).append("\n");
 		}
 		System.out.println(sb);
 	}
